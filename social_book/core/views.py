4e6dc9c430c4a546695abe440dcb645d993fcc3e
Piyush <impiyush016@gmail.com>
2022-05-24 17:02:22 +0530

    Templates and static files

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,3 +1,9 @@
+from curses.ascii import HT
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "index.html")
